66673a8a6984a0a58e1a52c3e728c61bc79cb6f8
Zhan Tong <zhantong94@gmail.com>
2023-03-07 17:22:59 +0800

    Lab 6.2

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: null };
+    this.greet = this.greet.bind(this);
+  }
+
+  greet(name) {
+    this.setState({ name: "John" });
+  }
   render() {
-    return;
-    <h1>Hello {this.props.name}</h1>;
+    return (
+      <div>
+        <h1>Hello {this.state.name == null ? "World" : this.state.name}</h1>;
+        <button onClick={this.greet}>Greet</button>
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<Greeting name="John" />, document.getElementById("root"));
+ReactDOM.render(<Greeting />, document.getElementById("root"));
